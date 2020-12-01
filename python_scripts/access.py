6258ec498d956359ef04a1c22b67ae31fbae6114
Andrew <andrew.wong.0101@gmail.com>
2020-12-01 14:39:45 +0800

    Reorganize structure

---
+++ python_scripts/access.py
@@ -0,0 +1,24 @@
+import numpy as np
+import random
+'''
+AlexNet
+
+Sparse mode
+
+Weight Sparsity:
+
+conv1:	   87%
+conv2:	 2.99%
+conv3:	 1.94%
+conv4:	 2.44%
+conv5:	 2.98%
+
+Activation Spasity:
+
+conv1:	   98%
+conv2:	 76.8%
+conv3:	   51%
+conv4:	   48%
+conv5:	   47%
+
+'''
